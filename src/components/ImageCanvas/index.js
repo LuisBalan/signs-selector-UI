1767ca3f4c6f275898a5e03b90e3e352897ff438
LuisBalan <jlbalcz@gmail.com>
2022-04-17 01:44:54 -0500

    add navbar and set project

---
+++ src/components/ImageCanvas/index.js
@@ -0,0 +1,11 @@
+import './ImageCanvas.css'
+
+function ImageCanvas(){
+    return(
+        <div className="Image-Container">
+
+        </div>
+    )
+}
+
+export default ImageCanvas;
---
+++ src/components/Navbar/index.js
@@ -0,0 +1,11 @@
+import './Navbar.css'
+
+function Navbar(){
+    return(
+        <nav className="Navbar">
+            <h2>Image App</h2>
+        </nav>
+    )
+}
+
+export default Navbar;
