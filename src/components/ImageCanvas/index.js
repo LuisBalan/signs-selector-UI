1cece35086eaf515d19503ccaef98afbb035441e
LuisBalan <jlbalcz@gmail.com>
2022-04-24 02:03:24 -0500

    setting fetch and isLoading state

--- src/components/ImageCanvas/index.js
+++ src/components/ImageCanvas/index.js
@@ -1,11 +1,35 @@
+import { useEffect, useState } from 'react'
 import './ImageCanvas.css'
 
 function ImageCanvas(){
-    return(
-        <div className="Image-Canvas">
+    const [isLoading, setIsLoading] = useState(true);
+    const [content, setContent] = useState();
 
-        </div>
-    )
+    useEffect(() => {
+        fetch('https://dog.ceo/api/breeds/image/random')
+            .then(res => res.json())
+            .then(dog => {
+                setContent(dog.message);
+                setIsLoading(false)})           
+    }, [])
+
+    if(isLoading){
+        return(
+            <div className='Loading-Canvas'>
+                <h1 className='Loading-Message'>Image Is Loading</h1>
+            </div>
+        )
+    }else{
+        return(
+            <div className="Image-Canvas">
+                <img className='dogImage'
+                alt=''
+                src={content}
+                />
+            </div>
+        )
+    }
+
 }
 
 export default ImageCanvas;
