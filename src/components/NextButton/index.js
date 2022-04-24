eb46e5d6491289dacd95b1bb321642b93d10a08b
LuisBalan <jlbalcz@gmail.com>
2022-04-24 16:18:39 -0500

    add hover and active effects to button

--- src/components/NextButton/index.js
+++ src/components/NextButton/index.js
@@ -1,6 +1,8 @@
+import './NextButton.css';
+
 export default function ActionButton({Action, Legend}){
     return(
-            <button onClick={Action}>
+            <button className = 'Action-Button' onClick={Action}>
                 {Legend}  
             </button>
     );
